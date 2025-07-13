03eaa12216b7ba22a8c41ba6bd0bd47837a0100c
suhyun_ <163711629+suhyun113@users.noreply.github.com>
2025-07-14 00:05:02 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.py
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.py
@@ -1,13 +1,10 @@
 def factorial(n):
-    if n == 1:
+    if n == 0:
         return 1
     else:
         return n * factorial(n-1)
 
 N = int(input())
-if N == 0:
-    result = 1
-else:
-    result = factorial(N)
+result = factorial(N)
 
 print(result)
